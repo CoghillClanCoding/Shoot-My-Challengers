783ff517dbbfcb006a5791bd0bb0dc69deff4cc8
Steven Coghill <76601239+BriarSMC@users.noreply.github.com>
2023-11-14 14:24:19 -0500

    Level 3 complete

---
+++ Scenes/Levels/level_3.gd
@@ -0,0 +1,32 @@
+extends Node2D
+class_name Level3
+
+#++
+# This is the Level 2 scene
+#
+#--
+
+#+
+# Properties
+#-
+
+# The following properties must be set in the Inspector by the designer
+@export var scaleFactor: float
+
+# The following are set based on the Inspector values
+
+#+
+# Virtual Godot methods
+#-
+
+func _ready() -> void:
+	Globals.scaleMe($PlayingArea, scaleFactor)		# Adjust how big we are
+	
+#+
+# Class specific methods
+#-
+
+# Temp to exit the scene for debugging
+func _input(event):
+	if event.is_action_pressed("ui_cancel"):
+		get_tree().quit()
